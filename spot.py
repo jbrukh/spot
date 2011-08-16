76d25c766414f7640c735d90128617849983672d
Jake Brukhman <jbrukh@gmail.com>
2011-08-15 22:35:18 -0400

    Made it work.

--- spot.py
+++ spot.py
@@ -1,1 +1,24 @@
-from BeatifulSoup import BeautifulSoup
+import urllib2
+import json
+import optparse
+
+SEARCH_URL = 'http://ws.spotify.com/search/1/%s.json?q=%s'
+
+def fetch_url(url):
+    result = urllib2.urlopen(url).read()
+    return json.loads(result)
+
+def main(opts, args):
+    url = SEARCH_URL % (opts.type, args[0])
+    js = fetch_url(url)
+    if opts.verbose:
+        print js
+    for item in js[opts.type+'s']:
+        print item['name']
+
+if __name__=='__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-t', '--type', dest='type', choices=('album', 'artist', 'track'), default='artist')
+    parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False)
+    opts, args = parser.parse_args()
+    main(opts, args)
